b1a95b69b49880c5d546acc9cd03dc2e2d8e8191
plgatto <plinio.gatto@gmail.com>
2020-05-16 16:54:56 +0200

    Add a first main implementation Add exception on wrong word set size

--- src/main/java/com/plgatto/textanalyzer/App.java
+++ src/main/java/com/plgatto/textanalyzer/App.java
@@ -1,9 +1,37 @@
 package com.plgatto.textanalyzer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
+
+import com.plgatto.textanalyzer.engine.TextAnalyzer;
+
 public class App 
 {
+	static TextAnalyzer textAnalyzer = new TextAnalyzer(); 
+	
     public static void main( String[] args )
     {
-    	// TODO
+        File file = new File("test.txt");
+        try {
+			String data = FileUtils.readFileToString(file, "UTF-8");
+			Map<String, Integer> result = textAnalyzer.analyze(data, 2);
+			
+			Map<String, Integer> sortedByCount = result.entrySet()
+	                .stream()
+	                .sorted(Map.Entry.comparingByValue())
+	                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));	
+			
+			for(String wordsSet : sortedByCount.keySet()) {
+				System.out.println(wordsSet + ": " + sortedByCount.get(wordsSet));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
--- src/main/java/com/plgatto/textanalyzer/engine/TextAnalyzer.java
+++ src/main/java/com/plgatto/textanalyzer/engine/TextAnalyzer.java
@@ -5,23 +5,28 @@ import java.util.Map;
 
 public class TextAnalyzer {
 
-	public Map<String, Integer> analyze(String text, int wordsSetSize) {
+	public Map<String, Integer> analyze(String text, int wordsSetSize) throws IllegalStateException {
 		Map<String, Integer> wordsSets = new HashMap<String, Integer>();
 		text = text.toLowerCase();
 		String[] subSentences = text.split("\\p{Punct}");
-		for(String sentence : subSentences) {
+		for (String sentence : subSentences) {
 //			System.out.println(sentence);
 			String[] words = sentence.split("\\s+");
-			for (int i = 0; i < words.length - 1; i++) {
+			for (int i = 0; i < words.length - wordsSetSize; i++) {
 				StringBuilder sb = new StringBuilder();
 				for (int k = 0; k < wordsSetSize; k++) {
-					sb.append(words[i+k]).append(" ");
+					sb.append(words[i + k]).append(" ");
 				}
 				String wordsSet = sb.toString().strip();
-				if(!wordsSets.containsKey(wordsSet)) {
+				if(wordsSet.split(" ").length!=3) {
+					throw new IllegalStateException("Words sets does not contains " + wordsSetSize + " words\n"
+							+ "Words set: " + wordsSet + "\n"
+							+ "Sentence: " + sentence);
+				}
+				if (!wordsSets.containsKey(wordsSet)) {
 					wordsSets.put(wordsSet, 1);
 				} else {
-					wordsSets.put(wordsSet, wordsSets.get(wordsSet)+1);
+					wordsSets.put(wordsSet, wordsSets.get(wordsSet) + 1);
 				}
 //				System.out.println(wordsSets.toString());
 			}
